8c89797c2d2503c77cb2ca45e3431b1034a52c4a
Nick Moreton <nickmoreton@me.com>
2025-06-26 09:02:15 +0100

    Lets not worry about backwards compatibility

--- wagtail_unveil/views.py
+++
@@ -1,4 +0,0 @@
-# Import all views from the views package for backwards compatibility
-# This maintains the existing import structure while the views are now organized in separate files
-
-from .views import *  # noqa: F403, F401
