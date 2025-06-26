483c35f408b7f5b6aa3a6c0a5523a175d6b4de88
Nick Moreton <nickmoreton@me.com>
2025-06-26 08:57:00 +0100

    Move views into single views folder

---
+++ wagtail_unveil/views/search_promotion_report.py
@@ -0,0 +1,77 @@
+from collections import namedtuple
+from io import StringIO
+
+from django.conf import settings
+from wagtail.admin.views.reports import ReportView
+from wagtail.admin.widgets.button import HeaderButton
+
+from ..helpers.search_promotion_helpers import get_search_promotions_urls
+
+
+class UnveilSearchPromotionReportView(ReportView):
+    index_url_name = "unveil_search_promotion_report"
+    index_results_url_name = "unveil_search_promotion_report_results"
+    header_icon = "search"
+    template_name = "wagtail_unveil/unveil_url_report.html"
+    results_template_name = "wagtail_unveil/unveil_url_report_results.html"
+    page_title = "Unveil Search Promotion URL's"
+    list_export = [
+        "id",
+        "model_name",
+        "url_type",
+        "url",
+    ]
+    export_headings = {
+        "id": "ID",
+        "model_name": "Model Name",
+        "url_type": "URL Type",
+        "url": "URL",
+    }
+    paginate_by = None
+    
+    def get_header_buttons(self):
+         return [
+            HeaderButton(
+                label="Run Checks",
+                icon_name="link",
+                attrs={
+                    "data-action": "check-urls",
+                },
+            ),
+        ]
+
+    def get_filterset_kwargs(self):
+        # Get the base queryset and pass it to the filterset
+        kwargs = super().get_filterset_kwargs()
+        kwargs["queryset"] = self.get_queryset()
+        return kwargs
+    
+    def get_base_queryset(self):
+        # Return the base queryset for the report
+        return self.get_queryset()
+
+    def get_queryset(self):
+        # Create a StringIO object to capture any output/errors
+        output = StringIO()
+        
+        # Create a named tuple to represent URL entries
+        UrlEntry = namedtuple('UrlEntry', ['id', 'model_name', 'url_type', 'url'])
+        
+        # Collect URLs from different helpers
+        all_urls = []
+        
+        # We'll use a counter for IDs
+        counter = 1
+        
+        # Get URLs from different sources using helper functions
+        # Get max_instances from settings with a default of 20 (search promotions can be numerous)
+        max_instances = getattr(settings, 'WAGTAIL_UNVEIL_MAX_INSTANCES', 1)
+        base_url = "http://localhost:8000"  # Default base URL
+        
+        # Collect search promotion URLs
+        search_promotion_urls = get_search_promotions_urls(output, base_url, max_instances, self.request.user)
+        for model_name, url_type, url in search_promotion_urls:
+            all_urls.append(UrlEntry(counter, model_name, url_type, url))
+            counter += 1
+            
+        return all_urls
