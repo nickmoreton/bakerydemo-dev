228f897e86d9c3f29046822bc19576d267c210b4
Nick Moreton <nickmoreton@me.com>
2025-06-27 23:01:05 +0100

    big refactor generic report

--- wagtail_unveil/viewsets/generic_report.py
+++ wagtail_unveil/viewsets/generic_report.py
@@ -1,13 +1,182 @@
 from collections import namedtuple
-from io import StringIO
 
+from django.apps import apps
 from django.conf import settings
-from django.urls import path
+from django.urls import NoReverseMatch, reverse, path
 from wagtail.admin.views.generic import IndexView
 from wagtail.admin.viewsets.base import ViewSet
 from wagtail.admin.widgets.button import HeaderButton
 
-from ..helpers.generic import get_generic_urls
+
+def get_add_url(model):
+    """Get the add URL for creating a new instance of the given model type."""
+    if not model:
+        return None
+    try:
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:add")
+    except NoReverseMatch:
+        return None
+
+
+def get_list_url(model):
+    """Get the list URL for the given model."""
+    if not model:
+        return None
+    try:
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:index")  # ModelViewSet uses 'index' not 'list'
+    except NoReverseMatch:
+        return None
+
+
+def get_edit_url(instance):
+    """Get the edit URL for the given instance."""
+    if not instance:
+        return None
+    try:
+        model = type(instance)
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:edit", args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_delete_url(instance):
+    """Get the delete URL for the given instance."""
+    if not instance:
+        return None
+    try:
+        model = type(instance)
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:delete", args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_copy_url(instance):
+    """Get the copy URL for the given instance."""
+    if not instance:
+        return None
+    try:
+        model = type(instance)
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:copy", args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_history_url(instance):
+    """Get the history URL for the given instance."""
+    if not instance:
+        return None
+    try:
+        model = type(instance)
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:history", args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_usage_url(instance):
+    """Get the usage URL for the given instance."""
+    if not instance:
+        return None
+    try:
+        model = type(instance)
+        model_name = model._meta.model_name
+        return reverse(f"{model_name}:usage", args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_generic_models():
+    """
+    Get all models that should be included in the generic report.
+    This reads from Django settings to get the list of models to include.
+    
+    Returns:
+        List of model classes
+    """
+    # Get the list of models from settings
+    generic_models_list = getattr(settings, 'WAGTAIL_UNVEIL_GENERIC_MODELS', [])
+    
+    models = []
+    for model_path in generic_models_list:
+        try:
+            app_label, model_name = model_path.rsplit('.', 1)
+            model = apps.get_model(app_label, model_name)
+            models.append(model)
+        except (LookupError, ValueError):
+            continue
+    
+    return models
+
+
+def get_generic_urls(base_url, max_instances=1):
+    """
+    Generate URLs for all generic models.
+    
+    Args:
+        base_url: Base URL for the site (e.g., "http://localhost:8000")
+        max_instances: Maximum number of instance URLs to generate per model
+        
+    Returns:
+        List of tuples: (model_name, url_type, url)
+    """
+    urls = []
+    
+    # Get all generic models from settings
+    generic_models = get_generic_models()
+    
+    for model in generic_models:
+        model_name = f"{model._meta.app_label}.{model.__name__}"
+        
+        # Get add URL for the model
+        add_url = get_add_url(model)
+        if add_url:
+            urls.append((model_name, "add", f"{base_url}{add_url}"))
+        
+        # Get list URL for the model
+        list_url = get_list_url(model)
+        if list_url:
+            urls.append((model_name, "list", f"{base_url}{list_url}"))
+        
+        # Get existing instances and create URLs for them
+        try:
+            instances = model.objects.all()
+            if max_instances:
+                instances = instances[:max_instances]
+        except Exception:
+            continue
+        
+        for instance in instances:
+            # Get edit URL
+            edit_url = get_edit_url(instance)
+            if edit_url:
+                urls.append((model_name, "edit", f"{base_url}{edit_url}"))
+            
+            # Get delete URL
+            delete_url = get_delete_url(instance)
+            if delete_url:
+                urls.append((model_name, "delete", f"{base_url}{delete_url}"))
+            
+            # Get copy URL
+            copy_url = get_copy_url(instance)
+            if copy_url:
+                urls.append((model_name, "copy", f"{base_url}{copy_url}"))
+            
+            # Get history URL
+            history_url = get_history_url(instance)
+            if history_url:
+                urls.append((model_name, "history", f"{base_url}{history_url}"))
+            
+            # Get usage URL
+            usage_url = get_usage_url(instance)
+            if usage_url:
+                urls.append((model_name, "usage", f"{base_url}{usage_url}"))
+                
+    return urls
 
 
 class UnveilGenericReportIndexView(IndexView):
@@ -18,14 +187,12 @@ class UnveilGenericReportIndexView(IndexView):
     paginate_by = None
 
     def get_queryset(self):
-        output = StringIO()
         UrlEntry = namedtuple("UrlEntry", ["id", "model_name", "url_type", "url"])
         all_urls = []
         counter = 1
         max_instances = getattr(settings, "WAGTAIL_UNVEIL_MAX_INSTANCES", 1)
         base_url = "http://localhost:8000"
-        user = self.request.user if self.request else None
-        generic_urls = get_generic_urls(output, base_url, max_instances, user)
+        generic_urls = get_generic_urls(base_url, max_instances)
         for model_name, url_type, url in generic_urls:
             all_urls.append(UrlEntry(counter, model_name, url_type, url))
             counter += 1
@@ -53,18 +220,7 @@ class UnveilGenericReportViewSet(ViewSet):
     menu_name = "unveil_generic_report"
     url_namespace = "unveil_generic_report"
     url_prefix = "unveil/generic-report"
-    export_filename = "generic_urls"
-    list_export = ["id", "model_name", "url_type", "url"]
-    export_headings = {
-        "id": "ID",
-        "model_name": "Model Name",
-        "url_type": "URL Type",
-        "url": "URL",
-    }
-
-    @property
-    def index_view_class(self):
-        return UnveilGenericReportIndexView
+    index_view_class = UnveilGenericReportIndexView
 
     def get_urlpatterns(self):
         return [
