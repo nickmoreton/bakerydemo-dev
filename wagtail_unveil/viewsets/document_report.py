c2cadd35fcb38baed8daaffcb1cab53796d198e4
nickmoreton <nickmoreton@me.com>
2025-06-27 17:03:00 +0100

    Big refactor od document report

--- wagtail_unveil/viewsets/document_report.py
+++ wagtail_unveil/viewsets/document_report.py
@@ -1,13 +1,62 @@
 from collections import namedtuple
-from io import StringIO
 
 from django.conf import settings
-from django.urls import path
+from django.urls import NoReverseMatch, path, reverse
 from wagtail.admin.views.generic import IndexView
 from wagtail.admin.viewsets.base import ViewSet
 from wagtail.admin.widgets.button import HeaderButton
+from wagtail.documents import get_document_model
 
-from ..helpers.document import get_document_urls
+
+def get_document_index_url():
+    try:
+        return reverse('wagtaildocs:index')
+    except NoReverseMatch:
+        return None
+
+def get_document_add_url():
+    try:
+        return reverse('wagtaildocs:add')
+    except NoReverseMatch:
+        return None
+
+def get_document_edit_url(document_id):
+    try:
+        return reverse('wagtaildocs:edit', args=[document_id])
+    except NoReverseMatch:
+        return None
+
+def get_document_delete_url(document_id):
+    try:
+        return reverse('wagtaildocs:delete', args=[document_id])
+    except NoReverseMatch:
+        return None
+
+def get_document_urls(base_url, max_instances, user=None):
+    """Return a list of tuples (model_name, url_type, full_url) for documents."""
+    urls = []
+    index_url = get_document_index_url()
+    if index_url:
+        urls.append(('wagtail.Document', 'index', f"{base_url}{index_url}"))
+    add_url = get_document_add_url()
+    if add_url:
+        urls.append(('wagtail.Document', 'add', f"{base_url}{add_url}"))
+    Document = get_document_model()
+    try:
+        documents = Document.objects.all()[:max_instances]
+        for document in documents:
+            document_model_name = f"wagtail.Document_{document.id}_{document.title}"
+            edit_url = get_document_edit_url(document.id)
+            if edit_url:
+                urls.append((document_model_name, 'edit', f"{base_url}{edit_url}"))
+            delete_url = get_document_delete_url(document.id)
+            if delete_url:
+                urls.append((document_model_name, 'delete', f"{base_url}{delete_url}"))
+    except Document.DoesNotExist:
+        pass
+    except (AttributeError, ValueError, TypeError):
+        pass
+    return urls
 
 
 class UnveilDocumentReportIndexView(IndexView):
@@ -22,32 +71,16 @@ class UnveilDocumentReportIndexView(IndexView):
     
     def get_queryset(self):
         """Generate the queryset for document URLs."""
-        # Create a StringIO object to capture any output/errors
-        output = StringIO()
-        
-        # Create a named tuple to represent URL entries
         UrlEntry = namedtuple('UrlEntry', ['id', 'model_name', 'url_type', 'url'])
-        
-        # Collect URLs from different helpers
         all_urls = []
-        
-        # We'll use a counter for IDs
         counter = 1
-        
-        # Get URLs from different sources using helper functions
-        # Get max_instances from settings with a default of 1
         max_instances = getattr(settings, 'WAGTAIL_UNVEIL_MAX_INSTANCES', 1)
-        base_url = "http://localhost:8000"  # Default base URL
-        
-        # Get current user from request
+        base_url = "http://localhost:8000"
         user = self.request.user if self.request else None
-        
-        # Collect document URLs
-        document_urls = get_document_urls(output, base_url, max_instances, user)
+        document_urls = get_document_urls(base_url, max_instances, user)
         for model_name, url_type, url in document_urls:
             all_urls.append(UrlEntry(counter, model_name, url_type, url))
             counter += 1
-            
         return all_urls
         
     def get_header_buttons(self):
@@ -61,48 +94,18 @@ class UnveilDocumentReportIndexView(IndexView):
                 },
             ),
         ]
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['object_list'] = self.get_queryset()
-        return context
 
 
 class UnveilDocumentReportViewSet(ViewSet):
     """
     ViewSet for Unveil Document reports using Wagtail's ViewSet pattern.
-    
-    This provides an alternative to the ReportView-based implementation,
-    offering more flexibility and better integration with Wagtail's admin interface.
     """
-    
-    # Basic ViewSet configuration
-    model = None  # We don't have a specific model as this is a report
     icon = "doc-full"
     menu_label = "Document"
     menu_name = "unveil_document_report"
     url_namespace = "unveil_document_report"
     url_prefix = "unveil/document-report"
-    
-    # Export configuration
-    export_filename = "document_urls"
-    list_export = [
-        "id",
-        "model_name", 
-        "url_type",
-        "url",
-    ]
-    export_headings = {
-        "id": "ID",
-        "model_name": "Model Name",
-        "url_type": "URL Type", 
-        "url": "URL",
-    }
-    
-    @property
-    def index_view_class(self):
-        """Return the view class for the index view."""
-        return UnveilDocumentReportIndexView
+    index_view_class = UnveilDocumentReportIndexView
     
     def get_urlpatterns(self):
         """Return the URL patterns for this ViewSet."""
@@ -110,7 +113,6 @@ class UnveilDocumentReportViewSet(ViewSet):
             path("", self.index_view_class.as_view(), name="index"),
             path("results/", self.index_view_class.as_view(), name="results"),
         ]
-
 
 # Create an instance of the ViewSet to be registered
 unveil_document_viewset = UnveilDocumentReportViewSet("unveil_document_report")
