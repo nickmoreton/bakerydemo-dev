385d75c8d4e9ee172816dee0b116a5a7dc23f42a
Nick Moreton <nickmoreton@me.com>
2025-06-27 23:02:37 +0100

    Big refactor user report

--- wagtail_unveil/viewsets/user_report.py
+++ wagtail_unveil/viewsets/user_report.py
@@ -1,13 +1,165 @@
 from collections import namedtuple
-from io import StringIO
 
 from django.conf import settings
-from django.urls import path
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.urls import NoReverseMatch, reverse, path
 from wagtail.admin.views.generic import IndexView
 from wagtail.admin.viewsets.base import ViewSet
 from wagtail.admin.widgets.button import HeaderButton
 
-from ..helpers.user import get_user_urls
+
+def get_user_add_url():
+    """Get the add URL for creating a new user."""
+    try:
+        return reverse('wagtailusers_users:add')
+    except NoReverseMatch:
+        return None
+
+
+def get_user_index_url():
+    """Get the index/list URL for users."""
+    try:
+        return reverse('wagtailusers_users:index')
+    except NoReverseMatch:
+        return None
+
+
+def get_user_edit_url(instance):
+    """Get the edit URL for the given user instance."""
+    if not instance:
+        return None
+    try:
+        return reverse('wagtailusers_users:edit', args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_user_delete_url(instance):
+    """Get the delete URL for the given user instance."""
+    if not instance:
+        return None
+    try:
+        return reverse('wagtailusers_users:delete', args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_group_add_url():
+    """Get the add URL for creating a new group."""
+    try:
+        return reverse('wagtailusers_groups:add')
+    except NoReverseMatch:
+        return None
+
+
+def get_group_index_url():
+    """Get the index/list URL for groups."""
+    try:
+        return reverse('wagtailusers_groups:index')
+    except NoReverseMatch:
+        return None
+
+
+def get_group_edit_url(instance):
+    """Get the edit URL for the given group instance."""
+    if not instance:
+        return None
+    try:
+        return reverse('wagtailusers_groups:edit', args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_group_delete_url(instance):
+    """Get the delete URL for the given group instance."""
+    if not instance:
+        return None
+    try:
+        return reverse('wagtailusers_groups:delete', args=[instance.pk])
+    except NoReverseMatch:
+        return None
+
+
+def get_user_urls(base_url, max_instances=5):
+    """
+    Generate URLs for Wagtail User and Group models.
+    
+    Args:
+        base_url: Base URL for the site (e.g., "http://localhost:8000")
+        max_instances: Maximum number of instance URLs to generate per model
+        
+    Returns:
+        List of tuples: (model_name, url_type, url)
+    """
+    urls = []
+    
+    # Get the User model
+    User = get_user_model()
+    user_model_name = f"{User._meta.app_label}.{User.__name__}"
+    
+    # Get user add URL
+    user_add_url = get_user_add_url()
+    if user_add_url:
+        urls.append((user_model_name, "add", f"{base_url}{user_add_url}"))
+    
+    # Get user index URL
+    user_index_url = get_user_index_url()
+    if user_index_url:
+        urls.append((user_model_name, "index", f"{base_url}{user_index_url}"))
+    
+    # Get existing user instances and create URLs for them
+    try:
+        user_instances = User.objects.all()
+        if max_instances:
+            user_instances = user_instances[:max_instances]
+    except Exception:
+        user_instances = []
+    
+    for instance in user_instances:
+        # Get edit URL
+        edit_url = get_user_edit_url(instance)
+        if edit_url:
+            urls.append((user_model_name, "edit", f"{base_url}{edit_url}"))
+        
+        # Get delete URL
+        delete_url = get_user_delete_url(instance)
+        if delete_url:
+            urls.append((user_model_name, "delete", f"{base_url}{delete_url}"))
+    
+    # Process Groups
+    group_model_name = f"{Group._meta.app_label}.{Group.__name__}"
+    
+    # Get group add URL
+    group_add_url = get_group_add_url()
+    if group_add_url:
+        urls.append((group_model_name, "add", f"{base_url}{group_add_url}"))
+    
+    # Get group index URL
+    group_index_url = get_group_index_url()
+    if group_index_url:
+        urls.append((group_model_name, "index", f"{base_url}{group_index_url}"))
+    
+    # Get existing group instances and create URLs for them
+    try:
+        group_instances = Group.objects.all()
+        if max_instances:
+            group_instances = group_instances[:max_instances]
+    except Exception:
+        group_instances = []
+    
+    for instance in group_instances:
+        # Get edit URL
+        edit_url = get_group_edit_url(instance)
+        if edit_url:
+            urls.append((group_model_name, "edit", f"{base_url}{edit_url}"))
+        
+        # Get delete URL
+        delete_url = get_group_delete_url(instance)
+        if delete_url:
+            urls.append((group_model_name, "delete", f"{base_url}{delete_url}"))
+            
+    return urls
 
 
 class UnveilUserReportIndexView(IndexView):
@@ -18,14 +170,12 @@ class UnveilUserReportIndexView(IndexView):
     paginate_by = None
 
     def get_queryset(self):
-        output = StringIO()
         UrlEntry = namedtuple("UrlEntry", ["id", "model_name", "url_type", "url"])
         all_urls = []
         counter = 1
         max_instances = getattr(settings, "WAGTAIL_UNVEIL_MAX_INSTANCES", 1)
         base_url = "http://localhost:8000"
-        user = self.request.user if self.request else None
-        user_urls = get_user_urls(output, base_url, max_instances, user)
+        user_urls = get_user_urls(base_url, max_instances)
         for model_name, url_type, url in user_urls:
             all_urls.append(UrlEntry(counter, model_name, url_type, url))
             counter += 1
@@ -53,18 +203,7 @@ class UnveilUserReportViewSet(ViewSet):
     menu_name = "unveil_user_report"
     url_namespace = "unveil_user_report"
     url_prefix = "unveil/user-report"
-    export_filename = "user_urls"
-    list_export = ["id", "model_name", "url_type", "url"]
-    export_headings = {
-        "id": "ID",
-        "model_name": "Model Name",
-        "url_type": "URL Type",
-        "url": "URL",
-    }
-
-    @property
-    def index_view_class(self):
-        return UnveilUserReportIndexView
+    index_view_class = UnveilUserReportIndexView
 
     def get_urlpatterns(self):
         return [
