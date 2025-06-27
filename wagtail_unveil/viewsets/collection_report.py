2251cfad4f2581a919fa7257c97902494dba0419
nickmoreton <nickmoreton@me.com>
2025-06-27 16:57:02 +0100

    Big refactor collections

--- wagtail_unveil/viewsets/collection_report.py
+++ wagtail_unveil/viewsets/collection_report.py
@@ -1,13 +1,11 @@
 from collections import namedtuple
-from io import StringIO
 
 from django.conf import settings
-from django.urls import path
+from django.urls import NoReverseMatch, path, reverse
 from wagtail.admin.views.generic import IndexView
 from wagtail.admin.viewsets.base import ViewSet
 from wagtail.admin.widgets.button import HeaderButton
-
-from ..helpers.collection import get_collection_urls
+from wagtail.models import Collection
 
 
 class UnveilCollectionReportIndexView(IndexView):
@@ -22,8 +20,6 @@ class UnveilCollectionReportIndexView(IndexView):
     
     def get_queryset(self):
         """Generate the queryset for collection URLs."""
-        # Create a StringIO object to capture any output/errors
-        output = StringIO()
         
         # Create a named tuple to represent URL entries
         UrlEntry = namedtuple('UrlEntry', ['id', 'model_name', 'url_type', 'url'])
@@ -43,7 +39,7 @@ class UnveilCollectionReportIndexView(IndexView):
         user = self.request.user if self.request else None
         
         # Collect collection URLs
-        collection_urls = get_collection_urls(output, base_url, max_instances, user)
+        collection_urls = get_collection_urls(base_url, max_instances, user)
         for model_name, url_type, url in collection_urls:
             all_urls.append(UrlEntry(counter, model_name, url_type, url))
             counter += 1
@@ -61,11 +57,6 @@ class UnveilCollectionReportIndexView(IndexView):
                 },
             ),
         ]
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['object_list'] = self.get_queryset()
-        return context
 
 
 class UnveilCollectionReportViewSet(ViewSet):
@@ -75,31 +66,13 @@ class UnveilCollectionReportViewSet(ViewSet):
     This provides an alternative to the ReportView-based implementation,
     offering more flexibility and better integration with Wagtail's admin interface.
     """
-    
-    # Basic ViewSet configuration
-    model = None  # We don't have a specific model as this is a report
+
     icon = "folder-open-1"
     menu_label = "Collection"
     menu_name = "unveil_collection_report"
     url_namespace = "unveil_collection_report"
     url_prefix = "unveil/collection-report"
     index_view_class = UnveilCollectionReportIndexView
-    
-    # Export configuration
-    export_filename = "collection_urls"
-    list_export = [
-        "id",
-        "model_name", 
-        "url_type",
-        "url",
-    ]
-    export_headings = {
-        "id": "ID",
-        "model_name": "Model Name",
-        "url_type": "URL Type", 
-        "url": "URL",
-    }
-
     
     def get_urlpatterns(self):
         """Return the URL patterns for this ViewSet."""
@@ -110,4 +83,53 @@ class UnveilCollectionReportViewSet(ViewSet):
 
 
 # Create an instance of the ViewSet to be registered
-unveil_collection_viewset = UnveilCollectionReportViewSet("unveil_collection_report")
+unveil_collection_viewset = UnveilCollectionReportViewSet()
+
+def get_collection_index_url():
+    try:
+        return reverse('wagtailadmin_collections:index')
+    except NoReverseMatch:
+        return None
+
+def get_collection_add_url():
+    try:
+        return reverse('wagtailadmin_collections:add')
+    except NoReverseMatch:
+        return None
+
+def get_collection_edit_url(collection_id):
+    try:
+        return reverse('wagtailadmin_collections:edit', args=[collection_id])
+    except NoReverseMatch:
+        return None
+
+def get_collection_delete_url(collection_id):
+    try:
+        return reverse('wagtailadmin_collections:delete', args=[collection_id])
+    except NoReverseMatch:
+        return None
+
+def get_collection_urls(base_url, max_instances, user=None):
+    """Return a list of tuples (model_name, url_type, full_url) for collections."""
+    urls = []
+    index_url = get_collection_index_url()
+    if index_url:
+        urls.append(('wagtail.Collection', 'index', f"{base_url}{index_url}"))
+    add_url = get_collection_add_url()
+    if add_url:
+        urls.append(('wagtail.Collection', 'add', f"{base_url}{add_url}"))
+    try:
+        collections = Collection.objects.exclude(depth=1)[:max_instances]
+        for collection in collections:
+            collection_model_name = f"wagtail.Collection_{collection.id}_{collection.name}"
+            edit_url = get_collection_edit_url(collection.id)
+            if edit_url:
+                urls.append((collection_model_name, 'edit', f"{base_url}{edit_url}"))
+            delete_url = get_collection_delete_url(collection.id)
+            if delete_url:
+                urls.append((collection_model_name, 'delete', f"{base_url}{delete_url}"))
+    except Collection.DoesNotExist:
+        pass
+    except (AttributeError, ValueError, TypeError):
+        pass
+    return urls
