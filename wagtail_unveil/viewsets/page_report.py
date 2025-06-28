532c35d99fe13b27e462d7e2195552f9d6e92da2
Nick Moreton <nickmoreton@me.com>
2025-06-28 17:34:27 +0100

    Refactored wagtail_unveil/viewsets/page_report.py to reduce the number of functions
    Also added a setting for WAGTAIL_UNVEIL_BASE_URL to allow customization of the base URL used in reports.

--- wagtail_unveil/viewsets/page_report.py
+++ wagtail_unveil/viewsets/page_report.py
@@ -1,5 +1,5 @@
-from collections import namedtuple
 
+from collections import namedtuple
 from django.conf import settings
 from django.urls import NoReverseMatch, path, reverse
 from wagtail.admin.views.generic import IndexView
@@ -8,127 +8,53 @@ from wagtail.admin.widgets.button import HeaderButton
 from wagtail.models import Page, get_page_models
 
 
-def get_page_add_url(app_label, model_name, parent_id):
-    """Get the add URL for creating a new page of the given model type."""
-    try:
-        return reverse('wagtailadmin_pages:add', args=[app_label, model_name, parent_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_edit_url(page_id):
-    """Get the edit URL for a page."""
-    try:
-        return reverse('wagtailadmin_pages:edit', args=[page_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_delete_url(page_id):
-    """Get the delete URL for a page."""
-    try:
-        return reverse('wagtailadmin_pages:delete', args=[page_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_copy_url(page_id):
-    """Get the copy URL for a page."""
-    try:
-        return reverse('wagtailadmin_pages:copy', args=[page_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_move_url(page_id):
-    """Get the move URL for a page."""
-    try:
-        return reverse('wagtailadmin_pages:move', args=[page_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_history_url(page_id):
-    """Get the history URL for a page."""
-    try:
-        return reverse('wagtailadmin_pages:history', args=[page_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_workflow_history_url(page_id):
-    """Get the workflow history URL for a page."""
-    try:
-        return reverse('wagtailadmin_pages:workflow_history', args=[page_id])
-    except NoReverseMatch:
-        return None
-
-def get_page_index_url(page_id):
-    """Get the index/list URL for a page (shows children/subpages)."""
-    try:
-        return reverse('wagtailadmin_explore', args=[page_id])
-    except NoReverseMatch:
-        return None
 
 def get_page_urls(base_url, max_instances):
     """Return a list of tuples (model_name, url_type, full_url) for pages."""
     urls = []
-    
-    # Get all Page models
     page_models = get_page_models()
-    
-    # Get root page for add URLs
     try:
         root_page = Page.objects.filter(depth=1).first()
     except Page.DoesNotExist:
         root_page = None
-    
     for model in page_models:
-        # Skip the base Page model
         if model._meta.label_lower == 'wagtailcore.page':
             continue
-            
         model_name = f"{model._meta.app_label}.{model.__name__}"
-        
         # Add URL (if we have a root page)
         if root_page:
-            add_url = get_page_add_url(model._meta.app_label, model._meta.model_name, root_page.pk)
-            if add_url:
+            try:
+                add_url = reverse('wagtailadmin_pages:add', args=[model._meta.app_label, model._meta.model_name, root_page.pk])
                 urls.append((model_name, 'add', f"{base_url}{add_url}"))
-        
+            except NoReverseMatch:
+                pass
         try:
-            # Get instances, prefer live pages if available
             if hasattr(model.objects, 'live'):
                 instances = model.objects.live()[:max_instances] if max_instances else model.objects.live()
             else:
                 instances = model.objects.all()[:max_instances] if max_instances else model.objects.all()
-            
             for instance in instances:
                 page_model_name = f"{model._meta.app_label}.{model.__name__}_{instance.id}_{instance.title}"
-                
                 # Admin URLs
-                edit_url = get_page_edit_url(instance.id)
-                if edit_url:
-                    urls.append((page_model_name, 'edit', f"{base_url}{edit_url}"))
-                
-                delete_url = get_page_delete_url(instance.id)
-                if delete_url:
-                    urls.append((page_model_name, 'delete', f"{base_url}{delete_url}"))
-                
-                copy_url = get_page_copy_url(instance.id)
-                if copy_url:
-                    urls.append((page_model_name, 'copy', f"{base_url}{copy_url}"))
-                
-                move_url = get_page_move_url(instance.id)
-                if move_url:
-                    urls.append((page_model_name, 'move', f"{base_url}{move_url}"))
-                
-                history_url = get_page_history_url(instance.id)
-                if history_url:
-                    urls.append((page_model_name, 'history', f"{base_url}{history_url}"))
-                
-                workflow_history_url = get_page_workflow_history_url(instance.id)
-                if workflow_history_url:
-                    urls.append((page_model_name, 'workflow_history', f"{base_url}{workflow_history_url}"))
-                
-                index_url = get_page_index_url(instance.id)
-                if index_url:
+                for url_type, url_name in [
+                    ('edit', 'wagtailadmin_pages:edit'),
+                    ('delete', 'wagtailadmin_pages:delete'),
+                    ('copy', 'wagtailadmin_pages:copy'),
+                    ('move', 'wagtailadmin_pages:move'),
+                    ('history', 'wagtailadmin_pages:history'),
+                    ('workflow_history', 'wagtailadmin_pages:workflow_history'),
+                ]:
+                    try:
+                        admin_url = reverse(url_name, args=[instance.id])
+                        urls.append((page_model_name, url_type, f"{base_url}{admin_url}"))
+                    except NoReverseMatch:
+                        pass
+                # Index URL
+                try:
+                    index_url = reverse('wagtailadmin_explore', args=[instance.id])
                     urls.append((page_model_name, 'index', f"{base_url}{index_url}"))
-                
+                except NoReverseMatch:
+                    pass
                 # Frontend view URL
                 if hasattr(instance, 'url') and instance.url:
                     view_url = instance.url
@@ -138,10 +64,8 @@ def get_page_urls(base_url, max_instances):
                         else:
                             view_url = f"{base_url}/{view_url}"
                     urls.append((page_model_name, 'view', view_url))
-                    
         except (model.DoesNotExist, AttributeError, ValueError, TypeError):
             pass
-    
     return urls
 
 
@@ -161,7 +85,7 @@ class UnveilPageReportIndexView(IndexView):
         all_urls = []
         counter = 1
         max_instances = getattr(settings, "WAGTAIL_UNVEIL_MAX_INSTANCES", 1)
-        base_url = "http://localhost:8000"
+        base_url = getattr(settings, "WAGTAIL_UNVEIL_BASE_URL", "http://localhost:8000")
         page_urls = get_page_urls(base_url, max_instances)
         for model_name, url_type, url in page_urls:
             all_urls.append(UrlEntry(counter, model_name, url_type, url))
